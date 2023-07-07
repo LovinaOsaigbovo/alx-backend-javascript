db00f13b2e0ccf4f52609f23841e556d1b18f479
Lovina Osaigbovo <111295018+LovinaOsaigbovo@users.noreply.github.com>
2023-07-07 06:48:11 -0700

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,54 +1,11 @@
-interface DirectorInterface {
-  workFromHome(): string;
-  getToWork(): string;
-  workDirectorTasks(): string;
+function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
 }
 
-interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
-}
-
-class Director implements DirectorInterface {
-  workFromHome(): string {
-    return 'Working from home';
-  }
-
-  getToWork(): string {
-    return 'Getting a coffee break';
-  }
-
-  workDirectorTasks(): string {
-    return 'Getting to director tasks';
-  }
-}
-
-class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return 'Cannot work from home';
-  }
-
-  getCoffeeBreak(): string {
-    return 'Cannot have a break';
-  }
-
-  workTeacherTasks(): string {
-    return 'Getting to work';
-  }
-}
-
-function createEmployee(salary: number | string): DirectorInterface | TeacherInterface {
-  if (typeof salary === 'number' && salary < 500) {
-    return new Teacher();
+function executeWork(employee: DirectorInterface | TeacherInterface): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
   } else {
-    return new Director();
+    return (employee as Teacher).workTeacherTasks();
   }
 }
-
-// Usage
-const employee1 = createEmployee(400);
-console.log(employee1.workFromHome()); // Output: Cannot work from home
-
-const employee2 = createEmployee(600);
-console.log(employee2.getToWork()); // Output: Getting a coffee break
