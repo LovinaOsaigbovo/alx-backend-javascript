008c3b8df300fb97368b40c47e5f009f89fcfdcc
Lovina Osaigbovo <111295018+LovinaOsaigbovo@users.noreply.github.com>
2023-07-07 07:26:05 -0700

    Update Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,1 +1,15 @@
+namespace Subjects {
+  export interface Teacher {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+  }
 
+  export class Subject {
+    private teacher: Teacher;
+
+    setTeacher(teacher: Teacher): void {
+      this.teacher = teacher;
+    }
+  }
+}
