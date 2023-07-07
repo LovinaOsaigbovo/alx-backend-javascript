a2ce131349046c5c96be59e6af756a603cdbeebf
Lovina Osaigbovo <111295018+LovinaOsaigbovo@users.noreply.github.com>
2023-07-07 06:13:25 -0700

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,1 +1,20 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  readonly fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
 
+// Usage
+const teacher: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: true,
+  yearsOfExperience: 5,
+  location: 'New York',
+  contract: true, // Adding a dynamic attribute
+};
+
+console.log(teacher);
