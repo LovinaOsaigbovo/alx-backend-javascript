de5a17d737aa7504f8a1f6bc156dc2006a23d9cb
Lovina Osaigbovo <111295018+LovinaOsaigbovo@users.noreply.github.com>
2023-07-07 06:34:23 -0700

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,1 +1,54 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getToWork(): string;
+  workDirectorTasks(): string;
+}
 
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getToWork(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+function createEmployee(salary: number | string): DirectorInterface | TeacherInterface {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+// Usage
+const employee1 = createEmployee(400);
+console.log(employee1.workFromHome()); // Output: Cannot work from home
+
+const employee2 = createEmployee(600);
+console.log(employee2.getToWork()); // Output: Getting a coffee break
